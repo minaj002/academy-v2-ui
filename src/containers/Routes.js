87a827fc688c2e5d214dd02747d44fcb1475df3b
Artis Jansons <artis.jansons@weststeincard.com>
2017-07-20 13:04:43 +0300

    Registration success page

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -6,6 +6,7 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import Login from '../pages/login/Login';
 import SignUp from '../pages/signup/SignUp';
+import SignUpSuccess from '../pages/signup/SignUpSuccess';
 import { requiresAuth } from './RequiresAuth';
 
 const Routes = (props) => {
@@ -17,7 +18,8 @@ const Routes = (props) => {
             {/*<Route exact path="/" component={requiresAuth(SignUp, isAuthenticated)} />*/}
 
             <Route exact path="/" component={SignUp} />
-            <Route path="/signup" component={SignUp} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route path="/signup/success" component={SignUpSuccess} />
             <Route path="/login" component={(props) => <Login {...props}
                   isAuthenticated={isAuthenticated} errorMessage={errorMessage} />} />
 
